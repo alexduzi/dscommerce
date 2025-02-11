464e22581444705af87ecd48711ce2a4f23b0f76
alexduzi <duzihd@gmail.com>
2025-02-10 21:15:05 -0300

    Adding BCryptPasswordEncoder config + update import.sql

--- src/main/java/com/alexduzi/dscommerce/DscommerceApplication.java
+++ src/main/java/com/alexduzi/dscommerce/DscommerceApplication.java
@@ -1,13 +1,20 @@
 package com.alexduzi.dscommerce;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DscommerceApplication {
+public class DscommerceApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DscommerceApplication.class, args);
-	}
+//    @Autowired
+//    private PasswordEncoder passwordEncoder;
 
+    public static void main(String[] args) {
+        SpringApplication.run(DscommerceApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+    }
 }
