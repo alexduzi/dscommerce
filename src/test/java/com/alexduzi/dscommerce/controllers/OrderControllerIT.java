4bb56dcd7e11ed2aeada89cc428b5df86abba747
alexduzi <duzihd@gmail.com>
2025-05-20 10:21:01 -0300

    OrderController integration tests

--- src/test/java/com/alexduzi/dscommerce/controllers/OrderControllerIT.java
+++ src/test/java/com/alexduzi/dscommerce/controllers/OrderControllerIT.java
@@ -10,10 +10,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.transaction.annotation.Transactional;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -48,12 +49,65 @@ class OrderControllerIT {
     @Test
     void findByIdShouldReturnOrderDTOWhenIdExistsAndAdminLogged() throws Exception {
         ResultActions result = mockMvc.perform(get("/orders/{id}", 1L)
-                .header("Authorization", "Bearer " + adminAccessToken)
+                        .header("Authorization", "Bearer " + adminAccessToken)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(MockMvcResultHandlers.print());
+
+        result.andExpect(status().isOk());
+        result.andExpect(jsonPath("$.id").value(1L));
+        result.andExpect(jsonPath("$.moment").value("2022-07-25T13:00:00Z"));
+        result.andExpect(jsonPath("$.status").value("PAID"));
+        result.andExpect(jsonPath("$.client").exists());
+        result.andExpect(jsonPath("$.client.name").value("Maria Brown"));
+        result.andExpect(jsonPath("$.payment").exists());
+        result.andExpect(jsonPath("$.items").exists());
+        result.andExpect(jsonPath("$.items[1].name").value("Macbook Pro"));
+        result.andExpect(jsonPath("$.total").value(1431.0));
+    }
+
+    @Test
+    void findByIdShouldReturnOrderDTOWhenIdExistsAndClientLogged() throws Exception {
+        ResultActions result = mockMvc.perform(get("/orders/{id}", 1L)
+                .header("Authorization", "Bearer " + clientAccessToken)
                 .accept(MediaType.APPLICATION_JSON));
 
         result.andExpect(status().isOk());
         result.andExpect(jsonPath("$.id").value(1L));
         result.andExpect(jsonPath("$.moment").value("2022-07-25T13:00:00Z"));
         result.andExpect(jsonPath("$.status").value("PAID"));
+        result.andExpect(jsonPath("$.client").exists());
+        result.andExpect(jsonPath("$.client.name").value("Maria Brown"));
+        result.andExpect(jsonPath("$.payment").exists());
+        result.andExpect(jsonPath("$.items").exists());
+        result.andExpect(jsonPath("$.items[1].name").value("Macbook Pro"));
+        result.andExpect(jsonPath("$.total").value(1431.0));
+    }
+
+    @Test
+    void findByIdShouldReturnForbiddenWhenIdExistsAndClientLoggedAndOrderDoesNotBelongUser() throws Exception {
+        ResultActions result = mockMvc.perform(get("/orders/{id}", 2L)
+                        .header("Authorization", "Bearer " + clientAccessToken)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(MockMvcResultHandlers.print());
+
+        result.andExpect(status().isForbidden());
+    }
+
+    @Test
+    void findByIdShouldReturnNotFoundWhenOrderIdDoesNotExistsAndAdminLogged() throws Exception {
+        ResultActions result = mockMvc.perform(get("/orders/{id}", 100L)
+                .header("Authorization", "Bearer " + adminAccessToken)
+                .accept(MediaType.APPLICATION_JSON));
+
+        result.andExpect(status().isNotFound());
+    }
+
+    @Test
+    void findByIdShouldReturnNotFoundWhenOrderIdDoesNotExistsAndClientLogged() throws Exception {
+        ResultActions result = mockMvc.perform(get("/orders/{id}", 100L)
+                .header("Authorization", "Bearer " + clientAccessToken)
+                .accept(MediaType.APPLICATION_JSON));
+
+        result.andExpect(status().isNotFound());
     }
 }
